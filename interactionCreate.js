51525db0824fef9dfe5ebb707f351d05b9044f2a
Masih <100484009+Masihdev1@users.noreply.github.com>
2023-03-01 03:54:48 -0800

    Create interactionCreate.js

---
+++ interactionCreate.js
@@ -0,0 +1,44 @@
+const {EmbedBuilder} = require("discord.js")
+module.exports = {
+	name: "interactionCreate",
+
+	async execute(interaction) {
+		if(interaction.isButton()){
+            if(interaction.customId === "Total Boosters"){
+                const TotalBoosters = await interaction.guild.members.fetch().then(
+                    (members) =>
+                      members
+                        .filter((m) => m.premiumSinceTimestamp)
+                        .map(
+                          (member) =>
+                            `${member} | ${member.user.username} | ${member.user.id}`
+                        )
+                        .join("\n") || "❌Sorry, No Boosters Found from this server!"
+                  );
+              
+                  const TotalBoosterEmbed = new EmbedBuilder()
+                    .setTitle(
+                      `All Server Boosters from ${
+                        interaction.guild.name
+                      } [${await interaction.guild.members
+                        .fetch()
+                        .then(
+                          (members) => members.filter((m) => m.premiumSinceTimestamp).size
+                        )}]`
+                    )
+                    .setDescription(TotalBoosters)
+                    .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
+                    .setColor(interaction.guild.members.me.displayHexColor)
+                    .setFooter({
+                      text: `Requested by ${interaction.user.username}`,
+                      iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+                    })
+                    .setTimestamp();
+                  await interaction.reply({
+                    embeds: [TotalBoosterEmbed],
+                    ephemeral: true,
+                  });
+            }
+        }
+	},
+};
