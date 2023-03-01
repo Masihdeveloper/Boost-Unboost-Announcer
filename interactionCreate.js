261b6633ddd4d255941e518810b3ae2b42f7a9e3
Masih <100484009+Masihdev1@users.noreply.github.com>
2023-03-01 08:31:19 -0800

    Update interactionCreate.js

--- interactionCreate.js
+++ interactionCreate.js
@@ -1,44 +1,48 @@
-const {EmbedBuilder} = require("discord.js")
+const { EmbedBuilder } = require("discord.js");
 module.exports = {
-	name: "interactionCreate",
+  name: "interactionCreate",
 
-	async execute(interaction) {
-		if(interaction.isButton()){
-            if(interaction.customId === "Total Boosters"){
-                const TotalBoosters = await interaction.guild.members.fetch().then(
-                    (members) =>
-                      members
-                        .filter((m) => m.premiumSinceTimestamp)
-                        .map(
-                          (member) =>
-                            `${member} | ${member.user.username} | ${member.user.id}`
-                        )
-                        .join("\n") || "❌Sorry, No Boosters Found from this server!"
-                  );
-              
-                  const TotalBoosterEmbed = new EmbedBuilder()
-                    .setTitle(
-                      `All Server Boosters from ${
-                        interaction.guild.name
-                      } [${await interaction.guild.members
-                        .fetch()
-                        .then(
-                          (members) => members.filter((m) => m.premiumSinceTimestamp).size
-                        )}]`
-                    )
-                    .setDescription(TotalBoosters)
-                    .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
-                    .setColor(interaction.guild.members.me.displayHexColor)
-                    .setFooter({
-                      text: `Requested by ${interaction.user.username}`,
-                      iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
-                    })
-                    .setTimestamp();
-                  await interaction.reply({
-                    embeds: [TotalBoosterEmbed],
-                    ephemeral: true,
-                  });
-            }
-        }
-	},
+  async execute(interaction) {
+    if (interaction.isButton()) {
+      if (interaction.customId === "Couple GIF") {
+        const TotalBoosters = await interaction.guild.members.fetch().then(
+          (members) =>
+            members
+              .filter((m) => m.premiumSinceTimestamp)
+              .map(
+                (member) =>
+                  `${member} | ${member.user.username} | ${
+                    member.user.id
+                  } | Boost at: <t:${Math.round(
+                    member.premiumSinceTimestamp / 1000
+                  )}:f>`
+              )
+              .join("\n") || "❌Sorry, No Boosters Found from this server!"
+        );
+
+        const TotalBoosterEmbed = new EmbedBuilder()
+          .setTitle(
+            `All Server Boosters from ${
+              interaction.guild.name
+            } [${await interaction.guild.members
+              .fetch()
+              .then(
+                (members) => members.filter((m) => m.premiumSinceTimestamp).size
+              )}]`
+          )
+          .setDescription(TotalBoosters)
+          .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
+          .setColor(interaction.guild.members.me.displayHexColor)
+          .setFooter({
+            text: `Requested by ${interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+          })
+          .setTimestamp();
+        await interaction.reply({
+          embeds: [TotalBoosterEmbed],
+          ephemeral: true,
+        });
+      }
+    }
+  },
 };
